5829bc11b944e4fbdbbe3911d1a1920762d0586f
maark1106 <mark1106@naver.com>
2024-03-20 09:58:17 +0900

    Service, Repository 분리

---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,50 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.response.UserResponse;
+import java.util.List;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean isUserNotExist(long id) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty(); // 결과가 하나라도 있다면 0, 없다면 비어있는 리스트
+    }
+
+    public void saveUser(String name, Integer age) {
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, name, age);
+    }
+
+    public List<UserResponse> getUsers(){
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    public void updateUserName(String name, long id) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    public void deleteUser(String name) {
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
+    public boolean isUserNotExist(String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+}
